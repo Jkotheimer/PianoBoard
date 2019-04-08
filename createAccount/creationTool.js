41bc7d613f2a33b3a054fab856a22e6b9c6aac97
Jack Kotheimer.edu <jkotheimer@luc.edu>
2019-04-08 12:03:01 -0500

    working on the creation tool

--- createAccount/creationTool.js
+++ createAccount/creationTool.js
@@ -1,12 +1,29 @@
+var genres = ["aleatoric music", "alternative country", "alternative dance", "alternative hip hop",
+    "alternative metal", "alternative rock", "ambient", "ambient house", "ambient music", "americana", "anarcho punk",
+    "anime music", "anti-folk", "apala", "ape haters", "arab pop", "arabesque", "arabic pop", "argentine rock", 
+    "ars antiqua", "ars nova", "art punk", "art rock"];
+
+var used = new Set([-1]);
 var pFade = setInterval(function() {
     // TODO get an API with a list of all the different genres
     // right here, add a part that 
-    document.getElementById("genres").placeholder = null
+    var rand = -1;
+    while(used.has(rand)) rand = Math.floor(Math.random() * genres.length);
+
+    document.getElementById("genres").placeholder = genres[rand];
 }, 2000);
 
 function grabSimilarGenres() {
-    var genres = readTextFile("../samples/Data/genres.txt");
-    console.log(genres);
+    //var genres = readTextFile("../samples/Data/genres.txt");
+    var input = document.getElementById("genres").nodeValue;
+    var autocomplete = document.getElementById("genre_autocomplete");
+    autocomplete.innerHTML = "FUCK";
+    for(let item of genres.entries()) {
+        if(item.includes(input)) {
+            console.log("ass");
+            autocomplete.innerHTML += item;
+        }
+    }
 }
 
 function readTextFile(file) {
