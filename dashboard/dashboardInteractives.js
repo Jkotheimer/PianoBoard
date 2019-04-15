0bbad06a7654e68b08f151821ecd70deef08486e
Jack Kotheimer.edu <jkotheimer@luc.edu>
2019-04-15 18:21:06 -0500

    dashboard style and interactivity updated

--- dashboard/dashboardInteractives.js
+++ dashboard/dashboardInteractives.js
@@ -1,37 +1,50 @@
 var animationin;
 var animation;
-var width
-function show(letter) {
-    clearInterval(animation);
-    var item = document.getElementById(letter);
-    item.innerHTML = null;
-    var title;
-    if(letter == "P") title = "Create New Project";
-    else if(letter == "T") title = "Tutorial";
-    else title = "Settings";
-    var i = 0;
-    animation = setInterval(function() {
-        item.innerText += title.charAt(i);
-        i++;
-        if(i == title.length) clearInterval(animation);
-    }, 20);
-}
-function hide(letter) {
-    clearInterval(animation);
-    var item = document.getElementById(letter);
-    var title = item.innerText;
-    if(title.length < 5) {
-        item.innerHTML = null;
-        return;
-    }
-    animation = setInterval(function() {
-        item.innerText = title;
-        title = title.slice(1);
-        if(title.length == 0) {
-            item.innerHTML = null;
-            clearInterval(animation);
+var width;
+var isOpen = false;
+var switches = document.getElementsByClassName("navLink");
+for(let i = 0; i < switches.length; i++) {
+    switches[i].addEventListener("mouseenter", function() {
+        if(isOpen) return;
+        isOpen = true;
+        for(let j = 0; j < switches.length; j++) {
+            switches[j].children[1].innerHTML = "";
         }
-    }, 10)
+        var letter = this.classList[1];
+        clearInterval(animation);
+        var item = document.getElementById(letter);
+        item.innerHTML = null;
+        var title;
+        if(letter == "P") title = "Create New Project";
+        else if(letter == "T") title = "Tutorial";
+        else title = "Settings";
+        var i = 0;
+        animation = setInterval(function() {
+            item.innerText += title.charAt(i);
+            i++;
+            if(i == title.length) clearInterval(animation);
+        }, 20);
+    });
+    switches[i].addEventListener("mouseleave", function() {
+        if(!isOpen) return;
+        isOpen = false;
+        clearInterval(animation);
+        var letter = this.classList[1];
+        var item = document.getElementById(letter);
+        var title = item.innerText;
+        if(title.length < 5) {
+            item.innerHTML = null;
+            return;
+        }
+        animation = setInterval(function() {
+            item.innerText = title;
+            title = title.slice(1);
+            if(title.length == 0) {
+                item.innerHTML = null;
+                clearInterval(animation);
+            }
+        }, 20)
+    });
 }
 
 var fade;
