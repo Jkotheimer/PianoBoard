a0ca589e82fdc97e13cfc58b9260dbe82cb297a5
Jack Kotheimer.edu <jkotheimer@luc.edu>
2019-04-22 20:24:55 -0500

    working on abstracting out all studio javascript to be made sense of

--- studio/KeyAction.js
+++ studio/KeyAction.js
@@ -5,54 +5,11 @@ var ucolor = null;
 var DKEYS = new Set();
 var reso = false;
 var isRec = false;
-var letterz = new Set(["tab","1","q","2","w","e","4","r","5","t","6","y","u","8","i","9","o","p","-","[",
-                        "=","]","backspace","\\","z","s","x","d","c","v","g","b","h","n","j","m",","]);
 var mouseIsDown = false;
 var keyIsDown = false;
 
-document.addEventListener("keydown", function(event){
-    keyIsDown = true;
-    var evkey = event.key.toLowerCase();
-    if(event.keyCode == 32 && !event.srcElement.id.includes("Tname") && !event.srcElement.id.includes("TEMPOOO")) {
-        event.preventDefault();
-    }
-    else if(evkey == "Shift") {
-        reso = true;
-        event.preventDefault();
-    }
-    else if(letterz.has(evkey) && !event.srcElement.id.includes("Tname") && 
-            !event.srcElement.id.includes("TEMPOOO") && !event.srcElement.id.includes("Numerator")){
-        action(event);
-    }
-    else if(event.key.includes("Arrow")) {
-        Scrub(event);
-    }
-  });
-document.addEventListener("keyup", function(event) {
-    keyIsDown = false;
-    var evkey = event.key.toLowerCase();
-    if (event.keyCode == 32) {
-        if(!event.srcElement.id.includes("Tname")) {
-            event.preventDefault();
-            togglePP(event);
-        }
-    }
-    else if(event.key == "Shift") {
-        reso = false;
-        event.preventDefault();
-    }
-    else if(event.key == "Delete") {
-        alertDeleteRec("recording", event);
-    }
-    if(!event.srcElement.id.includes("Tname") && !event.srcElement.id.includes("TEMPOOO")){
-        action(event);
-    }
-});
-document.addEventListener("mousedown", function() {mouseIsDown = true;});
-document.addEventListener("mouseup", function() {mouseIsDown = false;});
-window.addEventListener("contextmenu", e => {
-    e.preventDefault();
-  });
+
+
   
 function volFader(theNote) {
     var fadePoint = theNote.currentTime + 2;
