32f6e049245c0c7f1328a69821779c3ee34cbff7
jkotheimer <jkotheimer@luc.edu>
2020-03-20 21:55:53 -0500

    Fixed styling on landing page to fit new php modifications

--- _client/index.php
+++ _client/index.php
@@ -1,23 +1,28 @@
-﻿<!DOCTYPE html>
+﻿<?
+ob_start();
+?>
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="UTF-8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-		<title>Pianoboard</title>
+		<title>Pianoboard - <!--TITLE--></title>
 		<link rel="stylesheet" href="/resources/css/variables.css"/>
 		<link rel="stylesheet" href="/resources/css/logo.css"/>
 		<link rel="stylesheet" href="/resources/css/form_styles.css"/>
 		<link rel="stylesheet" href="/landing_page.css"/>
 	</head>
 	<body>
-		<?php
+		<?
+		$page = "login";
 		if(isset($_POST['submit'])) {
 			//code for the php form
-		}else {
-			$myfile = fopen("./resources/html/login.html", "r") or die("Unable to open file!");
-			echo fread($myfile,filesize("./resources/html/login.html"));
-			fclose($myfile);
+			$page = "dashboard";
 		}
+		$path = "./resources/html/" . $page . ".html";
+		$template = fopen($path, "r") or die("Unable to open file!");
+		echo fread($template,filesize($path));
+		fclose($template);
 		?>
 
 		<div class="info">
@@ -27,3 +32,9 @@
 		</div>
 	</body>
 </html>
+<?
+$page_contents = ob_get_contents();
+ob_end_clean();
+
+echo str_replace('<!--TITLE-->', $page, $page_contents);
+?>
