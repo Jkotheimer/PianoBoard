2a7ad07caae7082b2e93db9bc53d9178555e8b5b
jkotheimer <jkotheimer@luc.edu>
2020-03-23 02:50:57 -0500

    Added helper functions for dependency installation operations and error handling

---
+++ _scripts/functions.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+GREEN="\033[0;32m"
+BLUE="\033[0;34m"
+RED="\033[0;31m"
+NC="\033[0m"
+DONE="\r[${GREEN}DONE${NC}]\n"
+ERROR="\r[${RED}ERROR${NC}]"
+T="\t"
+
+handle_error() {
+	if [ ! $? -eq 0 ]; then
+		printf "${ERROR} ${1}\n"
+		exit -1
+	fi
+}
+
+exists() {
+	if [ $(command -v ${1}) ]; then
+		echo 1
+	fi
+}
+
+# ${1} : name of file to download
+# ${2} : url from which to download
+download_dependency() {
+	printf "${T}Downloading ${1}..."
+	if [ ! -f /tmp/${1}.tar.gz ]; then
+		handle_error $(curl -o /tmp/${1}.tar.gz ${2} 2>&1)
+	fi
+	printf ${DONE}
+}
+
+# ${1} : name of file to extract
+# ${2} : directory to extract to
+extract_dependency() {
+	printf "${T}Extracting ${1}..."
+	mkdir ${2}/${1}
+	handle_error $(tar -xvzf /tmp/${1}.tar.gz -C ${2} 2>&1 > /dev/null)
+	printf ${DONE}
+}
+
+# ${1} : directory of source to be installed
+# ${2} : configuration options
+# ${3} : post installation script
+install_dependency() {
+	printf "${T}Installing ${1##*/}..."
+	cd ${1}
+	handle_error $(./configure ${2} 2>&1 > /dev/null)
+	handle_error $(make 2>&1 > /dev/null)
+	handle_error $(make install 2>&1 > /dev/null)
+	${3}
+	rm -r ${1}
+	printf ${DONE}
+}
+
+# ${1} Line identifier
+# ${2} File name
+uncomment() {
+	sed -i "/${1}/s/^#//g" ${2}
+}
+
+# ${1} Process to find port for
+# search through netstat for the process, then isolate the port number and return it
+get_port() {
+	PORT=$(sudo netstat -tulpn | grep ${1} | awk -F' :::' '{print $2}')
+	if [ -z ${PORT} ]; then
+		PORT=$(sudo netstat -tulpn | grep ${1} | awk -F':' '{print $2}' | awk '{print $1}')
+	fi
+
+}
