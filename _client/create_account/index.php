8b45ed3b12690752a7dc32f6f5163d021c601d0f
jkotheimer <jkotheimer@luc.edu>
2020-04-02 11:37:00 -0500

    Began working on create account and login process

---
+++ _client/create_account/index.php
@@ -0,0 +1,85 @@
+﻿<?
+// Get the document root directory for complete file paths
+$ROOT = $_SERVER['DOCUMENT_ROOT'];
+// If the request was a post, check if a user with the given email exists
+// if not, create an account, create a token, set the cookies and redirect to the landing page
+$status = 1;
+$error = NULL;
+if(isset($_POST['submit']) && $_POST['submit'] == "Create Account") {
+	// Get the database connection and the secret pepper
+	require $ROOT . "/resources/php/database.phpsecret";
+	require $ROOT . "/resources/php/pepper.phpsecret";
+	
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$confirm_password = $_POST['confirm_password'];
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		$error = ["email_notification", "Invalid email"];
+	} else if(strlen($password) < 8) {
+		$error = ["password_notification", "Invalid password"];
+	} else if($password != $confirm_password) {
+		$error =  ["confirm_password_notification", "Passwords do not match"];
+	} else {
+		$status = 0;
+	}
+	unset($password, $confirm_password);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+		<title>Pianoboard - Create Account</title>
+		<style>	
+			 <?
+			echo fread(fopen($ROOT . "/resources/css/variables.css", 'r'), 
+				filesize($ROOT . "/resources/css/variables.css"));
+			echo fread(fopen($ROOT . "/resources/css/logo.css", 'r'), 
+				filesize($ROOT . "/resources/css/logo.css"));
+			echo fread(fopen($ROOT . "/resources/css/form_styles.css", 'r'), 
+				filesize($ROOT . "/resources/css/form_styles.css"));
+		       	 ?>
+		</style>
+		<link rel="stylesheet" href="/create_account/create_account.css"/>
+	</head>
+	<body>
+		<a class="logo_box" href="/">
+			<img src="/images/logo.png" class="logo"/>
+		</a>
+		<?
+		// Get the create account html file and render it here
+		if($status > 0) {
+			$path = $ROOT . "/resources/html/create_account.html";
+			$template = fopen($path, "r") or die("Unable to open file!");
+			$html = fread($template, filesize($path));
+			fclose($template);
+			if(isset($error)) {
+				$html = str_replace("<!--$error[0]-->", 
+					'<div id="email_notification" class="notification error">' . $error[1] . '</div>', 
+					$html);
+				if(isset($email)) {
+					$html = str_replace('placeholder="your.email@something.com"',
+						'value="' . $email . '"',
+						$html);
+				}	
+			}
+			echo $html;
+		} else {
+			// TODO Create an account
+			$hashed_password = password_hash($_POST['password'], PASSWORD_BCRYPT, ['salt' => $pepper]);
+			// TODO Push email and hashed password to database along with timestamp
+			$query = "INSERT INTO Account (Email, Username, Password, Creation_date, Is_private)
+					VALUES ('$email', '$email', '$hashed_password', 122, 0);";
+			$result = mysqli_query($database, $query);
+			if($result) {
+				// Success - redirect to home page
+			} else {
+				// Error - something went wrong (
+			}
+			$status = 0;
+		}
+		?>
+	</body>
+</html>	
--- _client/index.php
+++ _client/index.php
@@ -1,5 +1,35 @@
 ﻿<?
+$ROOT = $_SERVER['DOCUMENT_ROOT'];
 ob_start();
+// If the request was a POST, verify the email and password
+$page = "login";
+if(isset($_POST['email']) && isset($_POST['password'])) {
+	// Get the database connection and the secret pepper
+	require $ROOT . "/resources/php/database.phpsecret";
+	require $ROOT . "/resources/php/pepper.phpsecret";
+	// TODO use this in the account creation to hash the password and store it in the database
+	$email = $_POST['email'];
+	$query = "SELECT Password FROM Account WHERE Email='$email';";
+	$result = mysqli_query($database,$query);
+	// get number of rows returned 
+	if(mysqli_num_rows($result)) {
+		$row = mysqli_fetch_array($result);
+		$db_pass = $row['Password'];
+		echo 'Password: ' . $db_pass . '<br>';
+		if(password_verify($_POST['password'] . $pepper, $db_pass)) {
+			echo "Verified";
+			unset($_POST['password'], $db_pass, $row);	
+			$page = "dashboard";
+			// TODO get a token and set it as a cookie
+		} else {
+			echo "Invalid Password";
+		}
+	} else {
+		echo "Account with email $email does not exist";
+		// NO ACCOUNT WITH THIS EMAIL EXISTS
+	}
+	mysqli_close($database); 
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -9,52 +39,29 @@ ob_start();
 		<title>Pianoboard - <!--TITLE--></title>
 		<style>
 			<?
-			echo fread(fopen("./resources/css/variables.css", 'r'), filesize("./resources/css/variables.css"));
-			echo fread(fopen("./resources/css/logo.css", 'r'), filesize("./resources/css/logo.css"));
-			echo fread(fopen("./resources/css/form_styles.css", 'r'), filesize("./resources/css/form_styles.css"));
+			echo fread(fopen($ROOT . "/resources/css/variables.css", 'r'), 
+				filesize($ROOT . "/resources/css/variables.css"));
+			echo fread(fopen($ROOT . "/resources/css/logo.css", 'r'), 
+				filesize($ROOT . "/resources/css/logo.css"));
+			echo fread(fopen($ROOT . "/resources/css/form_styles.css", 'r'), 
+				filesize($ROOT . "/resources/css/form_styles.css"));
 			?>
 		</style>
 		<link rel="stylesheet" href="/landing_page.css"/>
 	</head>
 	<body>
 		<?
-		$page = "login";
-		if(isset($_POST['email']) && isset($_POST['password'])) {
-			require "./resources/php/database.phpsecret";
-			require "./resources/php/pepper.phpsecret";
-			$email = $_POST['email'];
-			$hash = password_hash($_POST['password'] . $pepper, PASSWORD_DEFAULT);
-			echo "HASHED PASSWORD: $hash<br>";
-			if(password_verify($_POST['password'] . $pepper, $hash)) {
-				echo "Verified!";
-			} else {
-				echo "Failed :(";
-			}
-			$query = "SELECT Password FROM Account WHERE Email='$email';";
-			$result = mysqli_query($database,$query);
-			$rows = mysqli_num_rows($result); 
-   			// get number of rows returned 
-
-			if ($rows) {     
-				while ($row = mysqli_fetch_array($result)) {         
-					echo 'ID: ' . $row['AccountID'] . '<br>';
-				}
-			} else {
-				// NO ACCOUNT WITH THIS EMAIL EXISTS
-			}
-			$page = "dashboard";
-			mysqli_close($database); 
-		}
-		$path = "./resources/html/" . $page . ".html";
+		// Get the body html file and render it here
+		$path = $ROOT . "/resources/html/" . $page . ".html";
 		$template = fopen($path, "r") or die("Unable to open file!");
-		echo fread($template,filesize($path));
+		echo fread($template, filesize($path));
 		fclose($template);
 		?>
 
 		<div class="info">
 			Developed and maintained by Jack Kotheimer
 			<br/>
-			To learn more about me, visit my <a href="http://67.162.86.57">personal website</a>
+			To learn more about me, visit my <a href="https://jackkotheimer.com">personal website</a>
 		</div>
 	</body>
 </html>
