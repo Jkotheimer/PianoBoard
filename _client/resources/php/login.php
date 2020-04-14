c3d9d4b0f771ac92f4085868ba390773c811ddc8
jkotheimer <jkotheimer@luc.edu>
2020-04-14 03:43:56 -0500

    Changed notification tags to be comments for php string replacement upon error

--- _client/resources/php/login.php
+++ _client/resources/php/login.php
@@ -1,10 +1,10 @@
 <style>
 	<?
 	$ROOT = $_SERVER['DOCUMENT_ROOT'];
-	echo fread(fopen($ROOT . "/resources/css/form_styles.css", 'r'),
-		filesize($ROOT . "/resources/css/form_styles.css"));
-	echo fread(fopen($ROOT . "/landing_page.css", "r"),
-		filesize($ROOT . "/landing_page.css"));
+	echo fread(fopen("$ROOT/resources/css/form_styles.css", 'r'),
+		filesize("$ROOT/resources/css/form_styles.css"));
+	echo fread(fopen("$ROOT/landing_page.css", "r"),
+		filesize("$ROOT/landing_page.css"));
 	?>
 </style>
 <div id="login">
@@ -17,18 +17,18 @@
 				<div class="form_label">Email</div>
 				<input type="text" id="email" name="email" class="form_input" placeholder="your.email@something.com"
 					autocapitalize="off" spellcheck="false" autocorrect="off"/>
-				<div id="email_notification" class="notification"></div>
+				<!--email_notification-->
 			</div>
 			<div class="form_section login_form_section">
 				<div class="form_label">Password</div>
 				<input type="password" id="password" name="password" class="form_input" placeholder="Enter your password"/>
-				<div id="password_notification" class="notification"></div>
+				<!--password_notification-->
 			</div>
 		</div>
 		<div class="link_area">
 			<a href="./create_account/" class="link create_account_link">Create a new Account</a>
 			<a href="./reset_password/" class="link reset_password_link">Forgot password?</a>
-			<input type="submit" class="link submit_link" value="Sign In"/>
+			<input type="submit" id="submit" name="submit" class="link submit_link" value="Sign In"/>
 		</div>
 	</form>
 </div>
