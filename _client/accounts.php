38e9cdcd8128832889df5fccd41216fc1ff7f2cd
jkotheimer <jkotheimer@luc.edu>
2020-04-14 20:33:37 -0500

    Added script to handle account info grabbing

---
+++ _client/accounts.php
@@ -0,0 +1,7 @@
+<?
+echo "ACCOUNTS PHP PAGE!!!<br/>\n";
+echo "Account: " . $_GET['account'] . "<br/>\n";
+echo "Project: " . $_GET['project'] . "<br/>\n";
+echo "Track: " . $_GET['track'] . "<br/>\n";
+echo "Recording: " . $_GET['recording'] . "<br/>\n";
+?>
