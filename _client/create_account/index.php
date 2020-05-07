cfedcaac1190fa2e47aaec51b2b6a8559637edfc
jkotheimer <jkotheimer@luc.edu>
2020-05-07 01:40:50 -0500

    Moved create account back to index file

--- _client/create_account/index.php
+++ _client/create_account/index.php
@@ -1,89 +1,76 @@
-<?php
-// root directory for complete file paths
-$ROOT = $_SERVER['DOCUMENT_ROOT'];
-
-// If the request was a post, check if a user with the given email exists
-// if not, create an account, create a token, set the cookies and redirect to the landing page
-$error = NULL;
-if( isset($_POST['submit']) && isset($_POST['email']) && 
-	isset($_POST['password']) && isset($_POST['confirm_password'])) {
-	
-	// Get the database connection and the secret pepper
-	require "$ROOT/resources/php/database.phpsecret";
-	require "$ROOT/resources/php/pepper.phpsecret";
-	require "$ROOT/resources/php/resources.php";
-	
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-	$confirm_password = $_POST['confirm_password'];
-
-	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-		$error = ["email_notification", "Invalid email"];
-	} else if(strlen($password) < 8) {
-		$error = ["password_notification", "Invalid password"];
-	} else if($password != $confirm_password) {
-		$error =  ["confirm_password_notification", "Passwords do not match"];
-	} else {
-		// No error - try to push to the database
-		$hashed_password = password_hash($password . $pepper, PASSWORD_BCRYPT);
-		$username = gen_username($email);
-		$query = "INSERT INTO Account (Email, Username, Password, Creation_date, Is_private)
-				VALUES ('$email', '$username', '$hashed_password', " . time() . ", " . False . ");";
-		if($database->query($query)) {
-			// Account successfully created - create a token, set it as a cookie, and redirect 
-
-			// Grab the ID of the account that we just created
-			$query = "SELECT AccountID FROM Account WHERE Email='$email' AND Username='$username';";
-			$AccountID = $database->query($query)->fetch_row()[0];
-
-			// Generate a random token and set the expiration date for a month from now
-			$Token = bin2hex(random_bytes(32));
-			$Expiration_date = time() + (86400 * 30);
-
-			// Push the token to the database and set it as a cookie
-			$query = "INSERT INTO Access_token (Token, AccountID, Expiration_date)
-					VALUES ('$Token', '$AccountID', '$Expiration_date');";
-			$database->query($query);
-			setcookie($session_cookie, $Token, $Expiration_date, "/");
-			setcookie($ID_cookie, $AccountID, $Expiration_date, "/");
-
-			// Redirect back to the root directory to handle the newly set cookie
-			exit(header("Location: http://" . $_SERVER['SERVER_NAME'], true, 301));
-		} else {
-			// Failure - account already exists
-			$error = ["email_notification", "An account with this email address already exists"];
-			http_response_code(400);
-		}
-	}
-	mysqli_close($database); 
-}
-ob_start();
-?>
 <!DOCTYPE html>
 <html>
-	<?
+	<?php
+	$ROOT = $_SERVER['DOCUMENT_ROOT'];
 	require "$ROOT/resources/php/head.php";
 	?>
 	<body>
+		<style>
+			<?php
+			$ROOT = $_SERVER['DOCUMENT_ROOT'];
+			echo fread(fopen($ROOT . "/resources/css/form_styles.css", 'r'),
+				filesize($ROOT . "/resources/css/form_styles.css"));
+			echo fread(fopen($ROOT . "/create_account/create_account.css", "r"),
+				filesize($ROOT . "/create_account/create_account.css"));
+			?>
+		</style>
+		
 		<a class="logo_box" href="/">
 			<img src="/images/logo.png" class="logo"/>
 		</a>
-		<? require "$ROOT/resources/php/create_account.php"; ?>
+		<div class="panel form_panel">
+			<h>Create An Account</h>
+	
+			<form class="form_area" method="POST" action="/api/auth/register">
+				<input type="hidden" id="platform" name="platform" value="web"/>
+				<div class="form_section">
+					<div class="form_label">Email</div>
+					<input type="text" id="email" name="email" class="form_input" placeholder="your.email@something.com"
+						autocapitalize="off" spellcheck="false" autocorrect="off" required
+						<?php
+						# If an email address was passed in the parameters, set it as the value of this input
+						if(isset($_GET['email'])) echo 'value="' . $_GET['email'] . '"';
+						?>
+					/>
+
+					<?php
+					# If an email notification was sent in the parameters, display it here
+					if(isset($_GET['email_notification'])) {
+						echo '<div id="email_notification" class="notification error">' . 
+							$_GET['email_notification'] . '</div>';
+					}
+					?>
+				</div>
+				<div class="form_section">
+					<div class="form_label">Password</div>
+					
+					<input type="password" id="password" name="password" 
+						class="form_input" placeholder="Choose a password" required/>
+
+					<?php
+					# If a password notification was sent in the parameters, display it here
+					if(isset($_GET['password_notification'])) {
+						echo '<div id="password_notification" class="notification error">' . 
+							$_GET['password_notification'] . '</div>';
+					}
+					?>
+				</div>
+				<div class="form_section">
+					<div class="form_label">Confirm Password</div>
+					
+					<input type="password" id="confirm_password" name="confirm_password" 
+						class="form_input" placeholder="Confirm Password" required/>
+
+					<?php
+					# If a password confirmation notification was sent in the parameters, display it here
+					if(isset($_GET['confirm_password_notification'])) {
+						echo '<div id="confirm_password_notification" class="notification error">' . 
+							$_GET['confirm_password_notification'] . '</div>';
+					}
+					?>
+				</div>
+				<input type="submit" id="submit" name="submit" value="Create Account"/>
+			</form>
+		</div>
 	</body>
 </html>	
-<?
-$html = ob_get_contents();
-ob_end_clean();
-
-if(isset($error)) {
-	$html = str_replace("<!--$error[0]-->", 
-		'<div id="' . $error[0] . '" class="notification error">' . $error[1] . '</div>', 
-		$html);
-	if(isset($email)) {
-		$html = str_replace('placeholder="your.email@something.com"',
-			'value="' . $email . '"',
-			$html);
-	}	
-}
-echo str_replace("<!--TITLE-->", "Create Account", $html);
-?>
