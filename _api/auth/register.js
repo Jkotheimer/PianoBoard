54816494d29966304005d0e5e07e6f4777d26962
Jack Kotheimer <jkotheimer@luc.edu>
2020-05-27 10:27:25 -0500

    Updated string concatenation and responses

--- _api/auth/register.js
+++ _api/auth/register.js
@@ -5,7 +5,7 @@ const crypto = require('./auth.js');
 
 module.exports = function(req, res) {
 	
-	const mysql = require(`${req.app.locals.root}/sql_connect.js`)
+	const mysql = require(`${req.app.locals.root}/sql_connect.js`);
 	
 	const email = req.body.email;
 	const password = req.body.password;
@@ -15,33 +15,33 @@ module.exports = function(req, res) {
 	if(!validator.validate(email)) {
 		// If the request was sent from a web browser, send a redirect back to the create account page
 		if(platform == 'web') {
-			res.status(302).set('Location', req.app.locals.host + '/create_account/' +
-				'?email_notification=Invalid%20email%20address' +
-				'&email=' + email)
+			res.redirect(`/create_account/
+				?email_notification=Invalid email address
+				&email=${email}`);
 		}
 		// If the request came from anywhere else, send back an error code with a json message
 		else {
-			res.status(400).json({'message': 'Invalid email address'})
+			res.status(400).json({message: 'Invalid email address'})
 		}
 	}
-	if(password.length < 10) {
+	if(password.length < 8) {
 		if(platform == 'web') {
-			res.redirect(req.app.locals.host + '/create_account/' +
-				'?password_notification=Password%20too%20weak' +
-				'&email=' + email);
+			res.redirect(`/create_account/
+				?password_notification=Password too weak
+				&email=${email}`);
 		}
 		else {
-			res.status(400).json({'message': 'Password too weak'});
+			res.status(400).json({message: 'Password too weak'});
 		}
 	}
 	if(password != confirm_password) {
 		if(platform == 'web') {
-			res.redirect(req.app.locals.host + '/create_account/' +
-				'?confirm_password_notification=Passwords%20do%20not%20match' +
-				'&email=' + email);
+			res.redirect(`/create_account/
+				?confirm_password_notification=Passwords do not match
+				&email=${email}`);
 		}
 		else {
-			res.status(400).json({'message': 'Passwords do not match'});
+			res.status(400).json({message: 'Passwords do not match'});
 		}
 	}
 
@@ -50,20 +50,19 @@ module.exports = function(req, res) {
 		const username = req.app.locals.resources.gen_username(_email);
 		const hashes = crypto.hash_password(_password)
 		const timestamp = req.app.locals.resources.new_date(0);
-		mysql.query("INSERT INTO Account (Email, Username, Password, Salt, Creation_date) VALUES ('" +
-			_email + "', '" + username + "', '" + hashes.hash + "', '" + 
-			hashes.salt + "', '" + timestamp + "');", 
+		mysql.query(`INSERT INTO Account (Email, Username, Password, Salt, Creation_date) VALUES 
+			('${_email}', '${username}', '${hashes.hash}', '${hashes.salt}', '${timestamp}');`, 
 			function(err, data) {
 				if(err) {
 					console.log(err)
 					// An account with this email already exists
 					if(platform == 'web') {
-						res.redirect('/create_account/' +
-							'?email_notification=Account with this email already exists' +
-							'&email=' + _email);
+						res.redirect(`/create_account/
+							?email_notification=Account with this email already exists
+							&email=${_email}`);
 					}
 					else {
-						res.status(400).json({'message': 'Account with this email already exists'});
+						res.status(400).json({message: 'Account with this email already exists'});
 					}
 				}
 				if(data) {
@@ -76,18 +75,17 @@ module.exports = function(req, res) {
 					res.cookie('pb_uid', account_id, {domain: req.app.locals.domain, path: '/', maxAge: req.app.locals.token_exp});
 					if(platform == 'web') {
 						res.redirect('/')
+					} else {
+						res.status(201).json({message: 'Account successfully created!'})
 					}
-					else {
-						res.status(201).json({'message': 'Account successfully created!'})
-					}
-				}
-			}
-		)
-	}
+				} // End data check
+			} // End account insertion callback
+		);
+	} // End create account
 
 	var set_token = function(account_id, token, exp_date) {
-		mysql.query("INSERT INTO Access_token (Token, AccountID, Expiration_date) VALUES ('"	+
-			token + "', '" + account_id + "', '" + exp_date + "');",
+		mysql.query(`INSERT INTO Access_token (Token, AccountID, Expiration_date) VALUES
+			('${token}', '${account_id}', '${exp_date}');`,
 			(err, data) => {if(err) throw err}
 		)
 	}
