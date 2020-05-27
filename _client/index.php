e2e47298cbd52918a343f3386157208d66c2c7b8
Jack Kotheimer <jkotheimer@luc.edu>
2020-05-27 08:43:04 -0500

    Abstracted cookie token auth to external php file

--- _client/index.php
+++ _client/index.php
@@ -1,77 +1,14 @@
 <?
 $ROOT = $_SERVER['DOCUMENT_ROOT'];
-header_remove();
+require "$ROOT/resources/php/auth.php";
+if(auth_token()) $page = "dashboard";
+else $page = "login";
 
-// Get the database connection and the secret pepper
-require "$ROOT/resources/php/database.phpsecret";
-require "$ROOT/resources/php/pepper.phpsecret";
-require "$ROOT/resources/php/resources.php";
-
-$page = "login";
-$error = NULL;
-$account = NULL;
-if(isset($_COOKIE[$session_cookie]) && isset($_COOKIE[$ID_cookie])) {
-	
-	// Attempt to grab the expiration date of the token from the set cookies
-	$Token = $_COOKIE[$session_cookie];
-	$AccountID = $_COOKIE[$ID_cookie];
-	$query = "SELECT Expiration_date from Access_token WHERE Token='$Token' AND AccountID='$AccountID';";
-	
-	$Expiration_date = new DateTime($database->query($query)->fetch_row()[0]);
-	$Expiration_date = $Expiration_date->getTimeStamp();
-
-	// If the query returns an actual value, load the dashboard
-	if($Expiration_date && time() < $Expiration_date) {
-		$page = "dashboard";
-		$GLOBALS['account'] = gen_account($AccountID);
-	} 
-	// If the query returns nothing, let it load the login page
-}
-// If the request was a POST, verify the email and password
-else if(isset($_POST['submit']) && isset($_POST['email']) && isset($_POST['password'])) {
-
-	$email = $_POST['email'];
-	$password = $_POST['password'];
-	$query = "SELECT AccountID, Password FROM Account WHERE Email='$email';";
-	$result = $database->query($query)->fetch_row();
-	$AccountID = $result[0];
-	$db_pass = $result[1];
-	if($db_pass) {
-		if(password_verify($password . $pepper, $db_pass)) {
-			// Successfully logged in - refresh token and load the dashboard
-
-			// Delete any tokens that may exist for this user
-			$query = "DELETE FROM Access_token WHERE AccountID='$AccountID'";
-			$database->query($query);
-
-			// Generate a new token and set the expiration date for a month from now
-			$Token = bin2hex(random_bytes(32));
-			$Expiration_date = time() + (86400 * 30);
-
-			// Push the token to the database and set it as a cookie
-			$query = "INSERT INTO Access_token (Token, AccountID, Expiration_date)
-					VALUES ('$Token', '$AccountID', '$Expiration_date');";
-			$database->query($query);
-			setcookie($session_cookie, $Token, $Expiration_date, "/");
-			setcookie($ID_cookie, $AccountID, $Expiration_date, "/");
-
-			$page = "dashboard";
-			$GLOBALS['account'] = gen_account($AccountID);
-		} else {
-			$error = ["password_notification", "Incorrect password"];			
-		}
-	} else {
-		$error = ["email_notification", "Account with email $email does not exist"];
-	}
-	mysqli_close($database); 
-}
 ob_start();
 ?>
 <!DOCTYPE html>
 <html>
-	<?
-	require "$ROOT/resources/php/head.php";
-	?>
+	<? require "$ROOT/resources/php/head.php"; ?>
 	<body>
 
 		<? require "$ROOT/resources/php/$page.php";?>
@@ -87,15 +24,5 @@ ob_start();
 $page_contents = ob_get_contents();
 ob_end_clean();
 
-if(isset($error)) {
-	$html = str_replace("<!--$error[0]-->", 
-		'<div id="' . $error[0] . '" class="notification error">' . $error[1] . '</div>', 
-		$html);
-	if(isset($email)) {
-		$html = str_replace('placeholder="your.email@something.com"',
-			'value="' . $email . '"',
-			$html);
-	}	
-}
 echo str_replace('<!--TITLE-->', $page, $page_contents);
 ?>
---
+++ _client/resources/php/auth.php
@@ -0,0 +1,30 @@
+<?php
+$ROOT = $_SERVER['DOCUMENT_ROOT'];
+
+// Get the database connection and the secret pepper
+require "$ROOT/resources/php/database.phpsecret";
+require "$ROOT/resources/php/pepper.phpsecret";
+require "$ROOT/resources/php/resources.php";
+
+function auth_token() {
+	if(isset($_COOKIE[$session_cookie]) && isset($_COOKIE[$ID_cookie])) {
+		
+		// Attempt to grab the expiration date of the token from the set cookies
+		$Token = $_COOKIE[$session_cookie];
+		$AccountID = $_COOKIE[$ID_cookie];
+		$query = "SELECT Expiration_date from Access_token WHERE Token='$Token' AND AccountID='$AccountID';";
+		
+		$Expiration_date = new DateTime($database->query($query)->fetch_row()[0]);
+		$Expiration_date = $Expiration_date->getTimeStamp();
+
+		mysqli_close($database); 
+	
+		// If the query returns an actual value, load the dashboard
+		if($Expiration_date && time() < $Expiration_date) {
+			$GLOBALS['account'] = gen_account($AccountID);
+			return true;
+		} 
+	}
+	return false;
+}	
+?>
