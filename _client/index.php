ceebd2f35c8317352403a354a9a1885114d510c8
Jack Kotheimer <jkotheimer@luc.edu>
2020-05-29 06:10:42 -0500

    Added logic to check for the responses from both login attempts and token auth attempts

--- _client/index.php
+++ _client/index.php
@@ -1,9 +1,15 @@
 <?
 $ROOT = $_SERVER['DOCUMENT_ROOT'];
 require "$ROOT/resources/php/auth.php";
-
-if(auth_token()) $page = "dashboard";
-else $page = "login";
+$page = 'login';
+if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login']) && isset($_POST['password'])) {
+	// Attempt to login
+	$result = login($_POST['login'], $_POST['password']);
+	if($result['status'] == 200) $page = 'dashboard';
+	else if($result['status'] == 404) $_GLOBALS['login_notification'] = $result['body']->message;
+	else if($result['status'] == 403) $_GLOBALS['password_notification'] = $result['body']->message;
+}
+else if(auth_token()) $page = 'dashboard';
 
 ob_start();
 ?>
@@ -12,7 +18,9 @@ ob_start();
 	<? require "$ROOT/resources/php/head.php"; ?>
 	<body>
 
-		<? require "$ROOT/resources/php/$page.php";?>
+		<?php
+		require "$ROOT/resources/php/$page.php";
+		?>
 
 		<div class="info">
 			Developed and maintained by Jack Kotheimer
