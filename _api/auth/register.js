529807f1651828878d8066cbf5ffe5480ebf772e
Jack Kotheimer <jkotheimer@luc.edu>
2020-05-31 02:42:17 -0500

    Updated registration to properly check for errors and send back multiple errors in json format if need be

--- _api/auth/register.js
+++ _api/auth/register.js
@@ -10,62 +10,35 @@ module.exports = function(req, res) {
 	const email = req.body.email;
 	const password = req.body.password;
 	const confirm_password = req.body.confirm_password
-	const platform = req.body.platform
+
+	var body = {message: {}};
 
 	if(!validator.validate(email)) {
 		// If the request was sent from a web browser, send a redirect back to the create account page
-		if(platform == 'web') {
-			res.redirect('/create_account/' +
-				'?email_notification=Invalid email address' +
-				`&email=${email}`);
-		}
-		// If the request came from anywhere else, send back an error code with a json message
-		else {
-			res.status(400).json({message: 'Invalid email address'})
-		}
+		body.message.email_notification = 'Invalid Email Address';
 	}
 	if(password.length < 8) {
-		if(platform == 'web') {
-			res.redirect('/create_account/' +
-				'?password_notification=Password too weak' +
-				`&email=${email}`);
-		}
-		else {
-			res.status(400).json({message: 'Password too weak'});
-		}
+		body.message.password_notification = 'Password too weak';
 	}
 	if(password != confirm_password) {
-		if(platform == 'web') {
-			res.redirect('/create_account/' +
-				'?confirm_password_notification=Passwords do not match' +
-				`&email=${email}`);
-		}
-		else {
-			res.status(400).json({message: 'Passwords do not match'});
-		}
+		body.message.confirm_password_notification = 'Passwords do not match';
 	}
+	if(body.message.size > 0) res.status(400).json(body);
 
 	// ALL IS WELL - ATTEMPT TO CREATE THE ACCOUNT
-	var create_account = function(_email, _password) {
+	async function create_account(_email, _password) {
 		const username = req.app.locals.resources.gen_username(_email);
 		const hashes = crypto.hash_password(_password)
 		const timestamp = req.app.locals.resources.new_date(0);
 		mysql.query(`INSERT INTO Account (Email, Username, Password, Salt, Creation_date) VALUES 
 			('${_email}', '${username}', '${hashes.hash}', '${hashes.salt}', '${timestamp}');`, 
 			function(err, data) {
-				if(err) {
-					console.log(err)
+				if(err || !data) {
 					// An account with this email already exists
-					if(platform == 'web') {
-						res.redirect('/create_account/' +
-							'?email_notification=Account with this email already exists' +
-							`&email=${_email}`);
-					}
-					else {
-						res.status(400).json({message: 'Account with this email already exists'});
-					}
+					body.message.email_notification = 'An account with this email already exists';
+					res.status(409).json(body);
 				}
-				if(data) {
+				else if(data) {
 					const token = crypto.gen_token();
 					// Token expires a month from today
 					const exp_date = req.app.locals.resources.new_date(req.app.locals.token_exp);
@@ -73,20 +46,28 @@ module.exports = function(req, res) {
 					set_token(account_id, token, exp_date);
 					res.cookie('pb_token', token, {domain: req.app.locals.domain, path: '/', maxAge: req.app.locals.token_exp});
 					res.cookie('pb_uid', account_id, {domain: req.app.locals.domain, path: '/', maxAge: req.app.locals.token_exp});
-					if(platform == 'web') {
-						res.redirect('/')
-					} else {
-						res.status(201).json({message: 'Account successfully created!'})
-					}
+					body.message =  'Account successfully created!';
+					res.status(201).json(body);
+					
 				} // End data check
+				else {
+					body.message = 'Something unexpected happened :/';
+					res.status(500).json(body);
+				}
 			} // End account insertion callback
 		);
+
 	} // End create account
 
 	var set_token = function(account_id, token, exp_date) {
 		mysql.query(`INSERT INTO Access_token (Token, AccountID, Expiration_date) VALUES
 			('${token}', '${account_id}', '${exp_date}');`,
-			(err, data) => {if(err) throw err}
+			(err, data) => {
+				if(err) {
+					body.message = 'Something went wrong :/ we cannot authenticate you right now';
+					res.status(500).json(body);
+				}
+			}
 		)
 	}
 
