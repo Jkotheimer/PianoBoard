4e6ec791ab75421a179b93004c7534307c720b2d
jkotheimer <jkotheimer@luc.edu>
2020-06-01 07:43:57 -0500

    Fixed username generation function to create the first username available given the prefix to the users email address

--- _api/resources.js
+++ _api/resources.js
@@ -1,11 +1,34 @@
 #!/usr/bin/node
 
-var gen_username = function(email) {
+const mysql = require('./sql_connect.js');
+
+// This function iterates through a list of usernames that already exist
+// If the username we want to add exists in the database already,
+// then add one to the number at the end of the username and try again
+var gen_username_recursive = function(err, data, username, num) {
+	if(err || !data) return;
+	var username_concat = num == 0 ? username : `${username}${num}`;
+	data.forEach((item, index) => {
+		console.log(`${item.Username} : ${username_concat}`);
+		if(item.Username == username_concat) {
+			console.log(`Restarting: ${username_concat}`);
+			username_concat = gen_username_recursive(err, data, username,  ++num);
+			return;
+		}
+	});
+	return username_concat;
+}
+
+async function gen_username(email) {
 	var username = email.split('@')[0];
-	var postfix = email.split('@')[1];
-	var sum = 0;
-	[...postfix].forEach(c => sum += c.charCodeAt(0));
-	return username + sum.toString();
+	var promise = new Promise((resolve, reject) => {
+		mysql.query(
+			`SELECT Username FROM Account WHERE Username REGEXP '^${username}';`,
+			(err, data) => resolve(gen_username_recursive(err, data, username, 0))
+		);
+	});
+	username = await promise;
+	return username;
 }
 
 var new_date = function(offset) {
