dbbd2d1dd3e9667f04c3a56c92b4d036276a8bc8
jkotheimer <jkotheimer@luc.edu>
2020-06-09 02:09:52 -0500

    Updating backend rendering for reset password page

--- _client/reset_password/index.php
+++ _client/reset_password/index.php
@@ -1,35 +1,45 @@
+<?php
+$ROOT = $_SERVER['DOCUMENT_ROOT'];
+$login = null;
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
+	$login = $_POST['login'];
+	echo 'Resetting password';
+	exit();
+}
+?>
 <!DOCTYPE html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Reset Password</title>
-	<link rel="shortcut icon" href="/UI/images/favicon-32.png" type="image/png" />
-	<link href="https://fonts.googleapis.com/css?family=Exo&display=swap" rel="stylesheet"/>
-	<link rel="stylesheet" href="/UI/css/logo.css"/>
-	<link rel="stylesheet" href="/UI/css/variables.css"/>
-	<link rel="stylesheet" href="reset_password.css">
-</head>
-<body>
-	<a class="logo_box" href="/">
-		<img src="/UI/images/logo.png" class="logo"/>
-	</a>
-	<div class="panel">
-		<h>Reset Password</h>
-		<form action="/reset_password" method="GET">
-			<div class="form_label">Enter your E-mail address</div>
-			<input type="text" name="email" class="form_input" placeholder="your.email@something.com" required/>
-			<div id="email_notification"></div>
-			<input type="submit" class="submit" value="Send Reset Link"/>
-		</form>
-	</div>
-	<div id="complete">Password reset link has been sent!<br><div id="timer"></div></div>
+<html>
+	<? require "$ROOT/resources/php/head.php"; ?>
+	<body>
+		<style>
+			<?php
+			echo fread(fopen("$ROOT/resources/css/form_styles.css", 'r'),
+				filesize("$ROOT/resources/css/form_styles.css"));
+			echo fread(fopen("$ROOT/resources/css/reset_password.css", "r"),
+				filesize("$ROOT/resources/css/reset_password.css"));
+			?>
+		</style>
+		<a class="logo_box" href="/">
+			<img src="/images/logo.png" class="logo"/>
+		</a>
+		<div class="panel">
+			<h class="panel_header">Reset Password</h>
+			<form action="/reset_password" method="GET">
+				<div class="form_label">Enter your E-mail address</div>
+				<input type="text" name="email" class="form_input" placeholder="your.email@something.com" required/>
+				<div id="email_notification"></div>
+				<input type="submit" class="submit" value="Send Reset Link"/>
+			</form>
+		</div>
+		<div id="complete">Password reset link has been sent!<br><div id="timer"></div></div>
 		
-	<script type="text/javascript">
-		for(let e of document.getElementsByTagName("input")) {
-			e.addEventListener("keyup", function() {
-				if(e.value.length > 1) e.classList.add("complete_form_input");
-				else e.classList.remove("complete_form_input");
-			});
-		}
-	</script>
-</body>
+		<script type="text/javascript">
+			for(let e of document.getElementsByTagName("input")) {
+				e.addEventListener("keyup", function() {
+					if(e.value.length > 1) e.classList.add("complete_form_input");
+					else e.classList.remove("complete_form_input");
+				});
+			}
+		</script>
+	</body>
+</html>
