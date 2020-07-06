0a04c31b70a584fc864ba743061811bd594d7a11
jkotheimer <jkotheimer@luc.edu>
2020-07-05 22:57:18 -0500

    Renamed directories without underscore

--- _client/new/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ROOT = $_SERVER['DOCUMENT_ROOT'];
-require "$ROOT/resources/php/auth.php"
-require "$ROOT/resources/php/resources.php"
-if($_SERVER['REQUEST_METHOD'] != 'POST') {
-	exit(header("Location: $domain"));
-}
-$
-?>
--- _client/resources/js/extra.js
+++
@@ -1,4 +0,0 @@
-// This file is to execute any preliminary instructions after dynamically grabbing a new HTML
-// element. These elements don't exist yet, so that's why we hold this on the server until they do
-
-function load_project_init() { focus_on(document.getElementById('project_name_input')); }
