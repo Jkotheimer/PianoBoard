805f7df49a016688556b6453c6d11867a019303d
jkotheimer <jkotheimer@luc.edu>
2020-07-11 00:56:40 -0500

    Split up node hook scripts. One downloads dependencies synchronously, while the other starts the node servlet and puts it to the background. This is the only way to properly start node inside a Docker container within a script

--- scripts/node_hook.sh
+++ scripts/node_hook.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env bash
+#!usr/bin/env bash
 
 [[ ! $(command -v node) || ! $(command -v npm) ]] && {
 	apt-get --yes update
 	apt-get --yes install nodejs npm
 }
 cd /api
-[ ! -d node_modules/ ] && npm install express cookie-parser fs path email-validator util mysql > /dev/null 2>&1
-node app.js > server.log 2>&1 &
+[ ! -d node_modules/ ] && {
+	npm install express cookie-parser fs path email-validator mysql
+}
+exit 0
---
+++ scripts/node_start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd /api
+node app.js >server.log 2>&1 
