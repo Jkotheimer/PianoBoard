dac8e9c96f9ec5cddae2016f1f97f69d9025e9c5
jkotheimer <jkotheimer@luc.edu>
2021-01-02 01:25:12 -0600

    Since Apache was replaced with Nginx, the conf files were replaced, and the deploy shell has updated to accommodate

--- config/dev/deploy.sh
+++ config/dev/deploy.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-# Copy all required config files
-cp /var/www/config/apache2.conf /etc/apache2/apache2.conf
-cp /var/www/config/000-default.conf /etc/apache2/sites-enabled/000-default.conf
-cp /var/www/config/php.ini /etc/php/7.4/apache2/php.ini
-
-# From mattraynor/lamp
-/run.sh 7.4
-
-#Start the api
+service php7.4-fpm status || service php7.4-fpm start
+service api status || service api start
+service mysql status || service mysql start
+service nginx status || service nginx start
 node /var/www/api/app.js &
-
